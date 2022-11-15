a452904892190ddc7e69681a4b47c329391467e1
Izumi <c0b210172b@edu.teu.ac.jp>
2022-11-15 15:03:28 +0900

    wip

--- ex06/hopping.py
+++ ex06/hopping.py
@@ -1,16 +1,25 @@
 import pygame as pg
-import sys
+import sys                    
 from random import randint
 
-sky = 0
 
-def collide(rct1, rct2): # 土台のRect、上に乗る鳥のRect、x速度、y速度
-    global sky
-    if rct1.top < rct2.bottom and rct1.bottom > rct2.top and rct1.left < rct2.right and rct1.right > rct2.left:
-        sky = 1
+sky = True #空にいるかどうかを判定するグローバル変数
+
+
+def collide(rct1, rct2, bird): #rct2がバード、rct1が足場
+    global sky           
+    if rct2.top < rct1.top and rct2.bottom < rct1.bottom and rct2.colliderect(rct1):
+
         rct2.bottom = rct1.top
+        sky = False
 
-def start(scr): #泉追加分　スタート画面
+def draw_score(scr, time):
+    fonto = pg.font.Font(None, 80)
+    score = time // 1000
+    txt = fonto.render(f"Score:{score}", True, "BLACK")
+    scr.sfc.blit(txt, (10, 10))
+
+def start(scr): #izumi追加分　スタート画面
     fonto = pg.font.Font(None, 60)
     txt = fonto.render("Press  SPACE  to  Start  Game", True, "BLACK")
     scr.sfc.blit(txt, (0,400))
@@ -25,45 +34,87 @@ class Screen:
         self.rct = self.sfc.get_rect()
         self.bgi_sfc = pg.image.load(bg_file)
         self.bgi_rct = self.bgi_sfc.get_rect()
+        self.bgi_sfc2 = pg.image.load(bg_file)
+        self.bgi_rct2 = self.bgi_sfc2.get_rect()
 
     def blit(self):
-        return self.sfc.blit(self.bgi_sfc, self.bgi_rct)
+        self.sfc.blit(self.bgi_sfc, self.bgi_rct)
+        return 
 
 
 class Bird:
-
-    global sky
 
     def __init__(self, bird_path, zup, default):
         self.sfc = pg.image.load(bird_path)
         self.sfc = pg.transform.rotozoom(self.sfc, 0, zup)
         self.rct = self.sfc.get_rect()
         self.rct.center = default[0], default[1]
-        self.vx = 0    #横方向の移動のための変数（着地したら0になる）
-        self.vy = 0    #上方向にどのくらいいけるかを溜める溜めに使う変数
-        self.vy2 = 1
-    
+
+        self.speed_y = 0
+        self.jump_power = 0
+        self.charge = False
+
     def blit(self, scr :Screen):
         return scr.sfc.blit(self.sfc, self.rct)
 
     def update(self, scr :Screen):
-        self.rct.move_ip(0, self.vy2)
+
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            self.vy += 1
-        if sky == 0:
+        if sky == True:
+
             if key_lst[pg.K_LEFT]:
-                self.rct.move_ip(-1, 0)
+                self.rct.move_ip(-3, 0)
             if key_lst[pg.K_RIGHT]:
-                self.rct.move_ip(+1, 0)
+                self.rct.move_ip(+3, 0)
+        self.wall_pass()
         self.blit(scr)
-    
+
+        #スピード制限
+        if self.speed_y < 3:
+            self.speed_y += 0.5
+
+        #y軸に動かす
+        self.rct.centery += self.speed_y
+        
+        pg.draw.rect(scr.sfc, (255, 0, 0), (self.rct.right, self.rct.centery, 20, 63))
+        pg.draw.rect(scr.sfc, (255, 255, 255), (self.rct.right, self.rct.centery, 20, 63-self.jump_power*-5))
+
+    #壁と天井の判定
+    def wall_pass(self):
+        if self.rct.right < 0:
+            self.rct.left = 600
+        if self.rct.left > 600:
+            self.rct.right = 0
+        if self.rct.top < 0:
+            self.rct.top = 0
+
+    def jump(self, event):
+        global sky
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_SPACE]:
+            #スピード制限
+            if self.jump_power > -20:
+                self.jump_power -= 2
+
+        #spacekeyを離したら跳ぶ
+        if sky == False and event.type == pg.KEYUP:
+            #少しの溜めでは跳ばない
+            if self.jump_power > -1 :
+                self.jump_power = 0
+            else:
+                sky = True
+                self.speed_y += self.jump_power
+                self.jump_power = 0
+            
+
 class FootFold:
 
+    global sky
+
     def __init__(self, y, scr :Screen):
-        self.sfc = pg.Surface((100, 20))
+        self.sfc = pg.Surface((125, 10))
         self.sfc.set_colorkey((0, 0, 0))
-        pg.draw.rect(self.sfc,(255, 0, 0),(10, 10, 100, 10))
+        pg.draw.rect(self.sfc,(255, 0, 0),(0, 0, 125, 10))
         self.rct = self.sfc.get_rect()
         self.y = y
         self.rct.centerx = randint(0, scr.rct.width)
@@ -73,21 +124,45 @@ class FootFold:
         return scr.sfc.blit(self.sfc, self.rct)
 
     def update(self, scr :Screen):
-        self.rct.move_ip(0, -1)
-        if self.rct.bottom < 0:
-            self.rct.centerx = randint(0, scr.rct.width)
-            self.rct.bottom = scr.rct.height
-        self.blit(scr)
 
+        self.rct.centery += 1
+        if self.rct.bottom > scr.rct.height:
+            self.rct.centerx = randint(0, scr.rct.width)
+            self.rct.bottom = 0
+        self.blit(scr)       
+
+
+class Text:
+
+    def __init__(self, txt):
+        self.txt = txt
+        self.fonts = []
+        for size in range(8, 300):
+            self.fonts.append(pg.font.Font(None, size))
+
+    def update(self, scr: Screen):
+        for font in self.fonts:
+            text = font.render(self.txt, True, (0,0,0))
+            pos = font.size(self.txt)
+            scr.blit()
+            scr.sfc.blit(text, (int((scr.wh[0]-pos[0])/2),int((scr.wh[1]-pos[1])/2)))
+            pg.display.update()
+            pg.time.wait(30)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    return
 
 def main():
-    scr = Screen("飛べ！こうかとん", (600, 800), "ex04/pg_bg.jpg")
-    bird = Bird(" fig/1.png", 2.0, (300, 400))
+    scr = Screen("飛べ！こうかとん", (600, 800), "fig/aozora.jpg")
+    bird = Bird("fig/1.png", 2.0, (300, 700))
     foot = FootFold(100, scr)
     foot1 = FootFold(300, scr)
     foot2 = FootFold(500, scr)
+    foot3 = FootFold(700, scr)
+    clock = pg.time.Clock()
+    starttime = True
     runflag = False
-
+    
     while runflag == False: #スタート画面
         scr.blit()
         start(scr)
@@ -99,33 +174,46 @@ def main():
                 if event.key == pg.K_SPACE:
                     runflag = True
 
-
     while runflag: #スタート画面から遷移した時にwhileに入る
         scr.blit()
         bird.update(scr)
         foot.update(scr)
         foot1.update(scr)
         foot2.update(scr)
+        foot3.update(scr)
+        
+        if starttime == True:
+            first_box = pg.draw.rect(scr.sfc, (255,255,255), (250,780,100,10))
+            collide(first_box, bird.rct, bird)
 
-        first_box = pg.draw.rect(scr.sfc, (255,255,255), (0,780,600,10))
-
-        collide(foot.rct, bird.rct)
-        collide(foot1.rct, bird.rct)
-        collide(foot2.rct, bird.rct)
-        collide(first_box, bird.rct)
+        collide(foot.rct, bird.rct, bird)
+        collide(foot1.rct, bird.rct, bird)
+        collide(foot2.rct, bird.rct, bird)
+        collide(foot3.rct, bird.rct, bird)
 
         for event in pg.event.get():
                 if event.type == pg.QUIT:
                     return
+                bird.jump(event)
 
+        if bird.rct.top > scr.rct.height:
+            break
+
+        time = pg.time.get_ticks()
+        draw_score(scr, time)
         pg.display.update() 
-        clock = pg.time.Clock()
-        clock.tick(1000)
+        clock.tick(120)
+        if time >= 5000:
+            starttime = False
+    
+    while (1):
+        gmov = Text(f"Score:{str(time // 1000)}")
+        gmov.update(scr)
+        return
+
 
 if __name__ == "__main__":
     pg.init() # 初期化
     main()
     pg.quit()
     sys.exit()
-
-    
